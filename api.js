55aedefb17ffbaf2784d3fd22dc3d8481f49c02d
Karel Zavadil <mojemejly@centrum.cz>
2023-01-06 20:30:27 +0000

    save to resources

--- api.js
+++ api.js
@@ -2,7 +2,7 @@ const cors = require("cors");
 const express = require("express");
 const fs = require("fs");
 
-const path = 'assets/resources.json';
+const path = 'src/resources.json';
 const port = 88;
 
 const corsOptions = {
